656670b2d5603a833772747ceb8eb52b920ec44a
Cheryl <banunu1011@gmail.com>
2024-07-09 14:34:35 +0800

    ran the thing wrong so rerunning it with the right configs

--- extratrainthin.sh
+++ extratrainthin.sh
@@ -9,11 +9,11 @@
 #SBATCH --gres=gpu:1              # Number of GPUs
 #SBATCH --mem=16G                 # Memory per node
 #SBATCH --time=02-00:00:00           # Time limit
-#SBATCH --nodelist=sist_gpu66
+#SBATCH --nodelist=sist_gpu65
 
 cd /public/home/chenhb/cheryl/implicit-shape-reconstruction/impl_recon
 
-python trainthick.py -p '/public/home/chenhb/cheryl/implicit-shape-reconstruction/paths_config_default.yml' -c '/public/home/chenhb/cheryl/implicit-shape-reconstruction/config_files/training/train_config_verse_thick_ad.yml'
+python trainthin.py -p '/public/home/chenhb/cheryl/implicit-shape-reconstruction/paths_config_default.yml' -c '/public/home/chenhb/cheryl/implicit-shape-reconstruction/config_files/training/train_config_verse_thin_ad.yml'
 
 # models dir is for long
 # model dir is from fri
